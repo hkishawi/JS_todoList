adf6d7ca55a427b433f41df46d8f40ef1206fb71
hkishawi <hkishawi@gmail.com>
2020-08-31 15:14:22 -0400

    started refactoring

---
+++ js/todoList.js
@@ -0,0 +1,71 @@
+var todoList = {
+  todos: [],
+  displayTodos: function () {
+    if (this.todos.length === 0) {
+      console.log('your list is empty!')
+    } else {
+      console.log('my todos: ')
+      for (var i = 0; i < this.todos.length; i++) {
+        // check if completed is true
+        if (this.todos[i].completed === true) {
+          console.log('(x) ', this.todos[i].todoText)
+        } else {
+          console.log('( ) ', this.todos[i].todoText)
+        }
+      }
+    }
+  },
+  addTodo: function (todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    })
+    this.displayTodos()
+  },
+  changeTodo: function (position, todoText) {
+    this.todos[position].todoText = todoText
+    this.displayTodos()
+  },
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1)
+    this.displayTodos()
+  },
+  toggleCompleted: function (position) {
+    var todo = this.todos[position]
+    todo.completed = !todo.completed
+    this.displayTodos()
+  },
+  toggleAll: function () {
+    var totalTodos = this.todos.length
+    var completedTodos = 0
+
+    // get number of completed todos.
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++
+      }
+    }
+    // Case 1: if everything is true, make everything false
+    // if completed Todos are same as number of total Todos, then make all todos False or unchecked
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false
+      }
+    } else {
+      // going through all todos
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true
+      }
+    }
+    this.displayTodos()
+  }
+}
+
+var handlers = {
+  displayTodos: function () {
+    todoList.displayTodos()
+  },
+  toggleAll: function () {
+    todoList.toggleAll()
+  }
+}
