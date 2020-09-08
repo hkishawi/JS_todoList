aba16d7b1a6d9f8dca187132970f0d31678a33f2
hkishawi <hkishawi@gmail.com>
2020-09-08 13:48:46 -0400

    clean up; v10 complete

--- js/todoList.js
+++ js/todoList.js
@@ -57,10 +57,8 @@ var handlers = {
     changeTodoTextInput.value = ''
     view.displayTodos()
   },
-  deleteTodo: function () {
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput')
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber)
-    deleteTodoPositionInput.value = ''
+  deleteTodo: function (position) {
+    todoList.deleteTodo(position)
     view.displayTodos()
   },
   toggleCompleted: function () {
@@ -90,8 +88,9 @@ var view = {
         todoTextWithCompletion = '( ) ' + todo.todoText
       }
 
-      todoLi.textContent = todoTextWithCompletion;
-      todoLi.appendChild(this.createDeleteButton());
+      todoLi.id = i
+      todoLi.textContent = todoTextWithCompletion
+      todoLi.appendChild(this.createDeleteButton())
       todosUl.appendChild(todoLi)
     }
   },
@@ -100,5 +99,16 @@ var view = {
     deleteButton.textContent = 'Delete'
     deleteButton.className = 'deleteButton'
     return deleteButton
+  },
+  setUpEventListeners: function () {
+    var todosUl = document.querySelector('ul')
+    todosUl.addEventListener('click', function (event) {
+      var elementClicked = event.target
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id))
+      }
+    })
   }
 }
+
+view.setUpEventListeners();
